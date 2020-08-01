b62503185159818da1dd2da07a94af2fe42b5ca8
chetan <chetanmsrit93@gmail.com>
2020-08-01 15:53:34 +0530

    Added farmers and seeds info

--- chatApp/src/app/home/home.component.ts
+++ chatApp/src/app/home/home.component.ts
@@ -7,7 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  seeds : any;
+  farmers : any;
+  
+  constructor() {
+    this.farmers = [{
+      name: 'AAA',
+      location: 'Vidyanagar, Bangalore',
+      seeds: 130
+    },{
+      name: 'AAA',
+      location: 'Vidyagiri, Bangalore',
+      seeds: 120
+    },{
+      name: 'AAA',
+      location: 'Vijyanagar, Bangalore',
+      seeds: 140
+    },{
+      name: 'AAA',
+      location: 'Vidyanagar, Bangalore',
+      seeds: 130
+    },{
+      name: 'AAA',
+      location: 'Vidyagiri, Bangalore',
+      seeds: 120
+    },{
+      name: 'AAA',
+      location: 'Vijyanagar, Bangalore',
+      seeds: 140
+    },{
+      name: 'AAA',
+      location: 'Vidyanagar, Bangalore',
+      seeds: 130
+    },{
+      name: 'AAA',
+      location: 'Vidyagiri, Bangalore',
+      seeds: 120
+    },{
+      name: 'AAA',
+      location: 'Vijyanagar, Bangalore',
+      seeds: 140
+    }]
+    this.seeds = [{
+      name : 'black-sesame',
+      image : 'https://image.shutterstock.com/image-photo/black-sesame-seeds-wooden-spoon-260nw-1421348225.jpg'
+    },{
+      name : 'black-sesame',
+      image : 'https://image.shutterstock.com/image-photo/black-sesame-seeds-wooden-spoon-260nw-1421348225.jpg'
+    },{
+      name : 'black-sesame',
+      image : 'https://image.shutterstock.com/image-photo/black-sesame-seeds-wooden-spoon-260nw-1421348225.jpg'
+    },{
+      name : 'black-sesame',
+      image : 'https://image.shutterstock.com/image-photo/black-sesame-seeds-wooden-spoon-260nw-1421348225.jpg'
+    }]
+   }
 
   ngOnInit(): void {
   }
